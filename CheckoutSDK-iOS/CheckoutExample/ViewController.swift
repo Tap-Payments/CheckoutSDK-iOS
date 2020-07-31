d0d678750aa3b353cf82d59d164f838e02daaa41
O Rabie <o.rabie@tap.company>
2020-07-31 12:27:29 +0200

    1- Empty framework for heckoit. 2- Emptt example project for checkout

---
+++ CheckoutSDK-iOS/CheckoutExample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CheckoutExample
+//
+//  Created by Osama Rabie on 7/31/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import UIKit
+import CheckoutSDK_iOS
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CheckoutSDK-iOS/CheckoutSDK-iOS/CheckoutSDK_iOS.h
@@ -0,0 +1,19 @@
+//
+//  CheckoutSDK_iOS.h
+//  CheckoutSDK-iOS
+//
+//  Created by Osama Rabie on 7/31/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheckoutSDK_iOS.
+FOUNDATION_EXPORT double CheckoutSDK_iOSVersionNumber;
+
+//! Project version string for CheckoutSDK_iOS.
+FOUNDATION_EXPORT const unsigned char CheckoutSDK_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheckoutSDK_iOS/PublicHeader.h>
+
+
