dc818aa4c35eafa2557953719c6b917c76ea969b
O Rabie <o.rabie@tap.company>
2020-08-17 12:32:33 +0200

    Added the Fawry to the demo.

--- CheckoutSDK-iOS/CheckoutSDK-iOS/Core/Logic/Interfaces/TapCheckoutSharedManager.swift
+++ CheckoutSDK-iOS/CheckoutSDK-iOS/Core/Logic/Interfaces/TapCheckoutSharedManager.swift
@@ -166,17 +166,19 @@ internal class TapCheckoutSharedManager {
         applePayChipViewModel.configureApplePayRequest()
         gatewayChipsViewModel.append(.init(tapChipViewModel: applePayChipViewModel, supportedCurrencies: [.AED,.USD,.SAR]))
         
-        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "KNET", icon: "https://meetanshi.com/media/catalog/product/cache/1/image/925f46717e92fbc24a8e2d03b22927e1/m/a/magento-knet-payment-354x.png") , supportedCurrencies: [.KWD]))
-        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "KNET", icon: "https://meetanshi.com/media/catalog/product/cache/1/image/925f46717e92fbc24a8e2d03b22927e1/m/a/magento-knet-payment-354x.png") , supportedCurrencies: [.KWD]))
-        
-        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "BENEFIT", icon: "https://media-exp1.licdn.com/dms/image/C510BAQG0Pwkl3gsm2w/company-logo_200_200/0?e=2159024400&v=beta&t=ragD_Mg4TUCAiVGiYOmjT2orY1IKEOOe_JEokwkzvaY") , supportedCurrencies: [.BHD]))
-        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "BENEFIT", icon: "https://media-exp1.licdn.com/dms/image/C510BAQG0Pwkl3gsm2w/company-logo_200_200/0?e=2159024400&v=beta&t=ragD_Mg4TUCAiVGiYOmjT2orY1IKEOOe_JEokwkzvaY") , supportedCurrencies: [.BHD]))
-        
-        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "SADAD", icon: "https://www.payfort.com/wp-content/uploads/2017/09/go_glocal_mada_logo_en.png") , supportedCurrencies: [.SAR]))
-        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "SADAD", icon: "https://www.payfort.com/wp-content/uploads/2017/09/go_glocal_mada_logo_en.png") , supportedCurrencies: [.SAR]))
-        
-        
         gatewayChipsViewModel.append(.init(tapChipViewModel: TapGoPayViewModel.init(title: "GoPay Clicked")))
+        
+        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "KNET", icon: "https://meetanshi.com/media/catalog/product/cache/1/image/925f46717e92fbc24a8e2d03b22927e1/m/a/magento-knet-payment-354x.png") , supportedCurrencies: [.KWD]))
+        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "KNET", icon: "https://meetanshi.com/media/catalog/product/cache/1/image/925f46717e92fbc24a8e2d03b22927e1/m/a/magento-knet-payment-354x.png") , supportedCurrencies: [.KWD]))
+        
+        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "BENEFIT", icon: "https://media-exp1.licdn.com/dms/image/C510BAQG0Pwkl3gsm2w/company-logo_200_200/0?e=2159024400&v=beta&t=ragD_Mg4TUCAiVGiYOmjT2orY1IKEOOe_JEokwkzvaY") , supportedCurrencies: [.BHD]))
+        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "BENEFIT", icon: "https://media-exp1.licdn.com/dms/image/C510BAQG0Pwkl3gsm2w/company-logo_200_200/0?e=2159024400&v=beta&t=ragD_Mg4TUCAiVGiYOmjT2orY1IKEOOe_JEokwkzvaY") , supportedCurrencies: [.BHD]))
+        
+        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "FAWRY", icon: "https://pwstg02.blob.core.windows.net/pwfiles/ContentFiles/8468Image.jpg") , supportedCurrencies: [.EGP]))
+        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "FAWRY", icon: "https://pwstg02.blob.core.windows.net/pwfiles/ContentFiles/8468Image.jpg") , supportedCurrencies: [.EGP]))
+        
+        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "SADAD", icon: "https://www.payfort.com/wp-content/uploads/2017/09/go_glocal_mada_logo_en.png") , supportedCurrencies: [.SAR]))
+        gatewayChipsViewModel.append(.init(tapChipViewModel: GatewayChipViewModel.init(title: "SADAD", icon: "https://www.payfort.com/wp-content/uploads/2017/09/go_glocal_mada_logo_en.png") , supportedCurrencies: [.SAR]))
         
         gatewayChipsViewModel.append(.init(tapChipViewModel:SavedCardCollectionViewCellModel.init(title: "•••• 1234", icon:"https://img.icons8.com/color/2x/amex.png")))
         gatewayChipsViewModel.append(.init(tapChipViewModel:SavedCardCollectionViewCellModel.init(title: "•••• 5678", icon:"https://img.icons8.com/color/2x/visa.png")))
