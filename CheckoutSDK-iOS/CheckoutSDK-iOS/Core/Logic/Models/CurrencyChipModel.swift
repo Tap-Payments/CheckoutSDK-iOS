61a33bf31026889935442dffdf4bcb176a1966cc
O Rabie <o.rabie@tap.company>
2020-08-17 11:17:50 +0200

    Create a new model to link the currency with the chips

---
+++ CheckoutSDK-iOS/CheckoutSDK-iOS/Core/Logic/Models/CurrencyChipModel.swift
@@ -0,0 +1,39 @@
+//
+//  CurrencyChipModel.swift
+//  CheckoutSDK-iOS
+//
+//  Created by Osama Rabie on 8/17/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import Foundation
+
+/// Represents a model that will link the different chips models with their approved currencies
+internal class CurrencyChipModel {
+    
+    /// Represents the chip view model itself (Apple, goPay & saved card)
+    lazy var tapChipViewModel:GenericTapChipViewModel = .init()
+    /// Represents the list of currencies the chip supports or should be visible when selected
+    lazy var supportedCurrencies:[TapCurrencyCode] = []
+    
+    /**
+     Creates a new instance that links the chip model with certain currencies
+     - Parameter tapChipViewModel: Represents the chip view model itself (Apple, goPay & saved card)
+     - Parameter supportedCurrencies: Represents the list of currencies the chip supports or should be visible when selected
+     */
+    init(tapChipViewModel:GenericTapChipViewModel, supportedCurrencies:[TapCurrencyCode]) {
+        self.tapChipViewModel = tapChipViewModel
+        self.supportedCurrencies = supportedCurrencies
+    }
+    
+    /**
+     Determines if the tap chip view model should be visible with a given currency
+     - Parameter currency: The currency to deteremine if the chip view model supports or no
+     - Returns: True if the list if currencies is empty (means it should be visible always.) or It has the given currency. Returns false otherwise
+     */
+    func enable(for currency:TapCurrencyCode) -> Bool {
+        // Make sure the currency list has values with the given currenct or it supports every currency
+        return (supportedCurrencies == []) || (supportedCurrencies.contains(currency))
+        
+    }
+}
