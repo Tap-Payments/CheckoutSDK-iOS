f62620cb56b7a553aa22e159f937aabc31a5e236
O Rabie <o.rabie@tap.company>
2020-11-15 11:26:19 +0200

    Added TapCustomerMetaData model

---
+++ CheckoutSDK-iOS/CheckoutSDK-iOS/Core/Logic/Network/Models/TapIntentRequest/TapCustomerMetaData.swift
@@ -0,0 +1,31 @@
+//
+//  TapCustomerMetaData.swift
+//  CheckoutSDK-iOS
+//
+//  Created by Osama Rabie on 11/15/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import Foundation
+/** Represents the model for parsing the metdata for a customer object coming from Tap backend
+ {
+    "metadata": {
+        "udf1": "test"
+    }
+ }
+ */
+struct TapCustomerMetaData : Codable {
+    /// this is whatever  metadat merchant can send i.e pspReference: This is a value we send you in the SALE and in the Refunds, under metadata.pspReference.
+    let udf1 : String?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case udf1 = "udf1"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        udf1 = try values.decodeIfPresent(String.self, forKey: .udf1)
+    }
+    
+}
