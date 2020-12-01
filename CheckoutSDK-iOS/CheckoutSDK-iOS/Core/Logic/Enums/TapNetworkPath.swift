d78e724a2bef852c76b490f9ee302e5356fca410
O Rabie <o.rabie@tap.company>
2020-12-01 18:03:50 +0200

    Adjusted the hiehgt of the card inpit when only one brand is supported

--- CheckoutSDK-iOS/CheckoutSDK-iOS/Core/Logic/Enums/TapNetworkPath.swift
+++ CheckoutSDK-iOS/CheckoutSDK-iOS/Core/Logic/Enums/TapNetworkPath.swift
@@ -11,7 +11,7 @@ import Foundation
 /// Represents a routing enum, which will has the end point of each needed request
 internal enum TapNetworkPath : String {
     /// Loading the Intent API
-    case IntentAPI = "5720fa1c-9b7e-4b68-810f-dbb79228405c"
+    case IntentAPI = "7b0b86c3-1e22-40f7-bf28-ad0ae58c391d" // case IntentAPI = "5720fa1c-9b7e-4b68-810f-dbb79228405c"
     /// Login to GoPay
     case GoPayLoginAPI = "7ffceaa7-0b86-4a18-88bb-c157c9a27aae"
 }
