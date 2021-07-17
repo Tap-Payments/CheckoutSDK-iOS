607195d689f9974fff404ea6fccada278e94b5a0
O Rabie <o.rabie@tap.company>
2021-07-17 10:00:43 +0200

    Code refactoring

--- CheckoutSDK-iOS/CheckoutSDK-iOS/Core/Logic/Extensions/Private/TapCheckout+CDNUrlsGeneration.swift
+++ CheckoutSDK-iOS/CheckoutSDK-iOS/Core/Logic/Extensions/Private/TapCheckout+CDNUrlsGeneration.swift
@@ -12,33 +12,41 @@ import Foundation
 extension PaymentOption {
     /// Computed attribute to get the CDN based URL
     internal var imageURL:URL {
-        // Compute the url based on the current user interface style
-        let interfaceStylePath:String = (UIScreen.main.traitCollection.userInterfaceStyle == .light) ? "" : "Dark"
-        return URL(string: "https://checkoutsdkios.b-cdn.net/\(CDNPath.PaymentOption.rawValue)\(interfaceStylePath)/\(identifier).png")!
+        return URL(string: "https://checkoutsdkios.b-cdn.net/\(CDNPath.PaymentOption.generateCDNPath())/\(identifier).png")!
     }
 }
 
 extension SavedCard {
     /// Computed attribute to get the CDN based URL
     internal var image:String {
-        // Compute the url based on the current user interface style
-        let interfaceStylePath:String = (UIScreen.main.traitCollection.userInterfaceStyle == .light) ? "" : "Dark"
-        return "https://checkoutsdkios.b-cdn.net/\(CDNPath.PaymentOption.rawValue)\(interfaceStylePath)/\(paymentOptionIdentifier ?? "").png"
+        return "https://checkoutsdkios.b-cdn.net/\(CDNPath.PaymentOption.generateCDNPath())/\(paymentOptionIdentifier ?? "").png"
     }
 }
 
 extension AmountedCurrency {
     /// Computed attribute to get the CDN based URL
     internal var cdnFlag:String {
-        // Compute the url based on the current user interface style
-        let interfaceStylePath:String = (UIScreen.main.traitCollection.userInterfaceStyle == .light) ? "" : "Dark"
-        return "https://checkoutsdkios.b-cdn.net/\(CDNPath.Currency.rawValue)\(interfaceStylePath)/\(currency.appleRawValue).png"
+        return "https://checkoutsdkios.b-cdn.net/\(CDNPath.Currency.generateCDNPath())/\(currency.appleRawValue).png"
     }
 }
 
+/// An enum to decide what is the pathway for different parts in the checkout sdks
 fileprivate enum CDNPath:String {
     
+    /// Will hold the path for gateways' and card brands' assets
     case PaymentOption  = "PaymentOption"
+    /// Will hold the path for currencies' assets
     case Currency       = "Currency"
+    
+    /**
+     Compute the asset's path depending on the current type and display mode
+     - Returns: The pathway for the given type( payment option, currency, etc.) and the current display mode (light or dark)
+     */
+    func generateCDNPath() -> String {
+        // Check first the display mode
+        let interfaceStylePath:String = (UIScreen.main.traitCollection.userInterfaceStyle == .light) ? "" : "Dark"
+        // Generate the correct path for the current type
+        return "\(rawValue)\(interfaceStylePath)"
+    }
     
 }
